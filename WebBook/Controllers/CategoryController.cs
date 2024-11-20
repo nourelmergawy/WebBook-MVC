28fe2273fbd00f3813811cf9256d56030171eff6
mrg <nouralmergawy@gmail.com>
2024-11-20 18:50:00 +0200

    finsh add Category

--- WebBook/Controllers/CategoryController.cs
+++ WebBook/Controllers/CategoryController.cs
@@ -16,5 +16,19 @@ namespace WebBook.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+
+        [HttpPost]
+     public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+
     }
 }
