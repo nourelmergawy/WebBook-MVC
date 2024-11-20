fe71d4d63e013471cdda5f34b4ce03a45526216a
mrg <mrg@DESKTOP-4H0ENJ4>
2024-11-20 18:11:52 +0200

    Add project files.

---
+++ WebBook/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBook.Data;
+using WebBook.Models;
+
+namespace WebBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
