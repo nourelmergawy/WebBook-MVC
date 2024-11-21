b6434f384239b947b06715068fcfd263c15c293c
mrg <nouralmergawy@gmail.com>
2024-11-21 14:24:26 +0200

    edit & delete ui , logic

--- WebBook/Controllers/CategoryController.cs
+++ WebBook/Controllers/CategoryController.cs
@@ -37,6 +37,61 @@ namespace WebBook.Controllers
             return View();
                 
         }
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
 
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? Id)
+        {
+            Category? obj = _db.Categories.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+
+        }
     }
 }
